88eebba0d2a3468244a021d92aaf6755aec3e0da
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-11 12:43:20 -0300

    Criando o formulário de cadrasto

---
+++ src/components/Formulario.vue
@@ -0,0 +1,29 @@
+<template>
+    <main>
+        <div>
+            <H1>Formulário De Casdrasto</H1>
+            
+
+
+        </div>
+    </main>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+    main {
+        display: flex;
+        place-items: center;
+        align-items: center;
+        height: 100vh;  
+        margin: 0;   
+        justify-content: center;
+        
+    }
+}
+
+
+
+
+</style>
