10bc3e5600f587ad9f2a543a2f487660e667bbc5
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-11 12:41:56 -0300

    Criando as rotas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import Header from './components/Header.vue';
-
+import Formulario from './components/Formulario.vue';
 </script>
 
 <template>
 <Header></Header>
+<Formulario></Formulario>
 </template>
 
 <style scoped>
