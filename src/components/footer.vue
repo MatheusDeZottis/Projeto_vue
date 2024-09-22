b912e3d0250006546b1052968da6c3557efd3a89
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-22 00:22:05 -0300

    Criando o rodape

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,12 @@
 <template>
     <footer>
-        
+        <div class="Rodape"> 
+         <p>&copy; Desenvolvido por 
+        <nav>
+             <a href="https://www.linkedin.com/in/matheus-de-zottis-2108b82b5/">Matheus De Zottis</a>
+        </nav>
+        </p>   
+        </div>
     </footer>
     
 
