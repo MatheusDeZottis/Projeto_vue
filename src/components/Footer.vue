010dd287f476c351a6f87a079170f894e392d5ea
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-28 13:31:35 -0300

    adiocinando o target blank para o link de ser aberto em uma nova guia

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    
+    <footer>
+        <div class="Rodape"> 
+         <p>&copy; Desenvolvido por 
+        <nav>
+             <a href="https://github.com/MatheusDeZottis" target="_blank">Matheus De Zottis</a>
+        </nav>
+        </p>   
+        </div>
+    </footer>
+    
+
+</template>
+
+
+<style scoped>
+
+.Rodape {
+    width: 100%;
+    text-align: center;
+    background-color:  rgb(50,205,50) ;
+
+}
+
+</style>
+
+<script>
+
+</script>
