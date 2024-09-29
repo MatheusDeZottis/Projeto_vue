456eb88169a698788fe5f8995a4176ff51024ec9
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-29 07:57:32 -0300

    criando a rota

--- src/router/router.js
+++ src/router/router.js
@@ -2,16 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Header from '@/components/Header.vue';
 import Login from '@/components/Login.vue';
 import Formulario from '@/components/Formulario.vue';
+import Footer from '@/components/Footer.vue'
 
 const routes = [
+  
   { path: '/header', component: Header },
   { path: '/login', component: Login },
-  { path: '/formulario', component: Formulario }
-];
+  { path: '/formulario', component: Formulario },
+  { path: '/footer', component: Footer }
+
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-});
+})
 
 export default router;
