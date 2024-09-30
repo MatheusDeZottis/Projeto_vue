65e88500be3fad3de3b3674e4bf85a56a385997f
MatheusDeZottis <matheusdezottis@gmail.com>
2024-09-30 00:30:43 -0300

    Ajuste de estilo no footer para prevenir sobreposição de elementos e adição de animação aos link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,8 @@
     
     <footer>
         <div class="Rodape"> 
-         <p>&copy; Desenvolvido por 
-        <nav>
+         <p id="text_rodape">&copy; Desenvolvido por 
+        <nav id="link_footer">
              <a href="https://github.com/MatheusDeZottis" target="_blank">Matheus De Zottis</a>
         </nav>
         </p>   
@@ -18,10 +18,63 @@
 
 .Rodape {
     width: 100%;
+    height: 60px;
+    margin-top: 22%;
+    margin-bottom: 2%;
+    margin-left: 0%;
+    margin-left: 0%;
     text-align: center;
-    background-color:  rgb(50,205,50) ;
+    background-color: #3dff00;
+    display: block;
+ 
 
 }
+
+
+a {
+    text-decoration: none;
+}
+
+#text_rodape {
+    color: black;
+    font-style: italic;
+    font-size: large;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    text-decoration: dashed;
+    
+}
+
+#link_footer {
+    position: relative;
+    display: inline-block;
+    color: #000000;
+    text-decoration: none;
+    font-size: 18px;
+    font-weight: bold;
+    transition: color 0.3s ease; 
+    
+}
+
+#link_footer::after {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: #ff3300;
+    transition: width 0.3s ease; 
+    
+}
+
+#link_footer:hover {
+    color: #0bff71; 
+}
+
+#link_footer:hover::after {
+    width: 100%; 
+}
+
 
 </style>
 
