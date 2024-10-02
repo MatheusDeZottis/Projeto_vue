fe968ea56c3513c17cfa9fe7b93ab36dceebc75a
MatheusDeZottis <matheusdezottis@gmail.com>
2024-10-01 23:40:25 -0300

    iMPORTANDO UMA TELA DE CARREGAMENTO EM BREVE TRAGO MAS DETALHES

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import Carregamento from './components/Carregamento.vue';
+
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <div>
+    <Carregamento></Carregamento>
     <Header ></Header>
     <router-view ></router-view>
     <Footer></Footer>
