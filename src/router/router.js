98e3130dfb24295c4f0cab48d068ae3076fa6efb
MatheusDeZottis <matheusdezottis@gmail.com>
2024-10-20 11:19:31 -0300

    Criando minha rota, para minha página principal

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Header from '@/components/Header.vue';
+import Home  from '@/components/Home.vue';
 import Login from '@/components/Login.vue';
 import Formulario from '@/components/Formulario.vue';
 import Footer from '@/components/Footer.vue'
 
+
 const routes = [
   
   { path: '/header', component: Header },
+  { path: '/Home', component: Home},
   { path: '/login', component: Login },
   { path: '/formulario', component: Formulario },
   { path: '/footer', component: Footer }
