8a7d079b791f1542bdd584e82c332a6879dd570d
MatheusDeZottis <matheusdezottis@gmail.com>
2024-12-19 00:46:19 -0300

    Criando o esqueleto do carrossel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,24 @@
 <template>
-    <RouterLink> Home </RouterLink>
+  
+   
     <h1>Seja bem-vindo ao meu portfólio!
         Aqui você encontra minhas experiências e projetos. </h1>
 
-    
+        <div class="carousel">
+    <div class="carousel-inner" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
+      <div class="carousel-item" v-for="(image, index) in images" :key="index">
+        <img :src="image" :alt="'Image ' + (index + 1)" />
+      </div>
+    </div>
+    <button class="carousel-control prev" @click="prevSlide">‹</button>
+    <button class="carousel-control next" @click="nextSlide">›</button>
+    </div>
 
 </template>
 
 <style scoped></style>
 
-<script setup >
+<script>
+
 
 </script>
